e8b5adaf20ceb7ce2197fd43263b81c6c3fd0b9d
Busra Demir <demirbusra003@icloud.com>
2026-04-18 17:22:40 +0300

    .

--- promise-ogrenimi.js
+++ promise-ogrenimi.js
@@ -1,31 +1,28 @@
-// Bu bizim "Zaman Alan" fonksiyonumuz. Hesaplama burada!
-const applyDamage = (damage, currentHP) => {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      const newHP = currentHP - damage; // Hesaplama burada yapılıyor
-      if (newHP <= 0) {
-        reject(`OYUNCU ÖLDÜ! Kalan: ${newHP}`);
-      } else {
-        resolve(`Oyuncu yaşadı. Kalan HP: ${newHP}`); // Dönen değer bu!
+const fetchUser= (id) => {
+  return new Promise((resolve, reject)=> {
+    setTimeout(() =>{
+      if(id ===1){
+      resolve({id:1, name: "Büşra D."});
       }
-    }, 2000);
+      else{
+        reject("kullanici bulunamadi")
+      }
+    } , 2000);
   });
 };
 
-async function savaşıBaşlat() {
-  console.log("Savaş başladı, 2 saniye bekliyoruz...");
+const fetchKonum = (userName) =>{
+  return new Promise ((resolve) => {
+    setTimeout(()=> {
+      resolve(`${userName} şu an İstanbulda`);
+    }, 1000);
+  });
+};
 
-  try {
-    // applyDamage içindeki resolve'dan gelen metin direkt 'sonuç'a atanır
-    const sonuç = await applyDamage(120, 100); 
-    console.log("Gelen Mesaj:", sonuç);
-  } catch (hata) {
-    // applyDamage içindeki reject'ten gelen metin buraya düşer
-    console.log("Gelen Hata:", hata);
-  }
-
-  console.log("Savaş bitti, şimdi bu satır yazıldı.");
-}
-
-savaşıBaşlat();
-console.log("Bak! Ben fonksiyonun dışındayım ve fonksiyonu beklemeden hemen yazıldım!");
+const fetchInfo = (loc) =>{
+  return new Promise ((resolve) =>{
+    setTimeout(() =>{
+      resolve("Tr-East-1");
+    } ,1001);
+});
+} 
